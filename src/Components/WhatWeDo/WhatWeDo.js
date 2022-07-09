a64ba223fa02b4559fe26b1e14f8e217b474c311
shahadat2772 <shahadat2772@gmailcom>
2022-07-09 12:23:37 +0600

    added a simple version of slider

--- src/Components/WhatWeDo/WhatWeDo.js
+++ src/Components/WhatWeDo/WhatWeDo.js
@@ -1,9 +1,18 @@
 import React from "react";
+import { Splide, SplideSlide } from "@splidejs/react-splide";
 
 const WhatWeDo = () => {
   return (
     <div>
-      <h2>HELLO FROM WHAT WE DO</h2>
+      <h2>What we do</h2>
+      <Splide aria-label="My Favorite Images">
+        <SplideSlide>
+          <img src="image1.jpg" alt="Image 1" />
+        </SplideSlide>
+        <SplideSlide>
+          <img src="image2.jpg" alt="Image 2" />
+        </SplideSlide>
+      </Splide>
     </div>
   );
 };
