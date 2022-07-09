006e474c06023268a7f38cde3a0a4f266f74ac7d
shahadat2772 <shahadat2772@gmailcom>
2022-07-09 17:45:44 +0600

    a slider added with images

--- src/Components/WhatWeDo/WhatWeDo.js
+++ src/Components/WhatWeDo/WhatWeDo.js
@@ -1,18 +1,73 @@
 import React from "react";
-import { Splide, SplideSlide } from "@splidejs/react-splide";
+import { Splide, SplideTrack, SplideSlide } from "@splidejs/react-splide";
+import portfolioimg1 from "../../images/portfolio/portfolio-01.jpg";
+import portfolioimg2 from "../../images/portfolio/portfolio-02.jpg";
+import portfolioimg3 from "../../images/portfolio/portfolio-03.jpg";
+import portfolioimg4 from "../../images/portfolio/portfolio-04.jpg";
+import portfolioimg5 from "../../images/portfolio/portfolio-05.jpg";
+import "./WhatWeDo.css";
+
+import "@splidejs/react-splide/css";
 
 const WhatWeDo = () => {
   return (
-    <div>
-      <h2>What we do</h2>
-      <Splide aria-label="My Favorite Images">
-        <SplideSlide>
-          <img src="image1.jpg" alt="Image 1" />
-        </SplideSlide>
-        <SplideSlide>
-          <img src="image2.jpg" alt="Image 2" />
-        </SplideSlide>
-      </Splide>
+    <div className="whatWeDoContainer">
+      <div>
+        <Splide
+          id="splideContainer"
+          options={{
+            type: "loop",
+            perPage: 5,
+            focus: "center",
+            drag: false,
+            arrows: true,
+            pagination: false,
+            perMove: 1,
+            autoplay: true,
+            start: 3,
+            speed: 800,
+            autoHeight: true,
+            breakpoints: {
+              640: {
+                perPage: 2,
+              },
+              480: {
+                perPage: 1,
+              },
+            },
+          }}
+          hasTrack={false}
+          aria-label="My Favorite Images"
+        >
+          <SplideTrack>
+            <SplideSlide>
+              <div>
+                <img src={portfolioimg1} alt="Image 1" />
+              </div>
+            </SplideSlide>
+            <SplideSlide>
+              <div>
+                <img src={portfolioimg2} alt="Image 2" />
+              </div>
+            </SplideSlide>
+            <SplideSlide>
+              <div>
+                <img src={portfolioimg3} alt="Image 3" />
+              </div>
+            </SplideSlide>
+            <SplideSlide>
+              <div>
+                <img src={portfolioimg4} alt="Image 4" />
+              </div>
+            </SplideSlide>
+            <SplideSlide>
+              <div>
+                <img src={portfolioimg5} alt="Image 5" />
+              </div>
+            </SplideSlide>
+          </SplideTrack>
+        </Splide>
+      </div>
     </div>
   );
 };
