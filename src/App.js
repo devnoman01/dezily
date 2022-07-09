b554a425175732d5aa3e11971e4278bdcf1191ab
shahadat2772 <shahadat2772@gmailcom>
2022-07-09 18:39:03 +0600

    made the what we do section mobile responsive

--- src/App.js
+++ src/App.js
@@ -2,11 +2,7 @@ import "./App.css";
 import WhatWeDo from "./Components/WhatWeDo/WhatWeDo";
 
 function App() {
-  return (
-    <div className="App">
-      <WhatWeDo></WhatWeDo>
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
