38828bd58135d1fad396e88514f885d437c513b4
mahfuzswaron <mahfuzswaron@gmail.com>
2022-07-09 12:11:01 +0600

    basic html structure completed

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,10 +1,50 @@
 import React from 'react';
+import img from '../../images/about/about.png';
 
 const About = () => {
     return (
-        <div>
-            this is about
-        </div>
+        <section>
+            <div>
+                <figure>
+                    <img src={img} alt="about" />
+                </figure>
+            </div>
+            <div>
+                <p>ABOUT Dezily</p>
+                <h2>Heart Surgery Specialist Expert Doctors
+                </h2>
+                <p>Chest pain is the most common warning sign of a heart attack. But there can be other symptoms, too, like lightheadedness of breath.
+                </p>
+                <div>
+                    <div>
+                        <div>icon here</div>
+                        <div>
+                            <h4>Heart Transplante</h4>
+                            <p>Heart symptoms call us for immediate</p>
+                        </div>
+                    </div>
+
+                </div>
+                <hr />
+                <ul>
+                    <li>
+                        icon
+                        <span>Breathing difficulties can be caused by many different conditions.
+                        </span>
+                    </li>
+                </ul>
+
+                <div>
+                    <button>
+                        Get Appointment
+                    </button>
+                    <button>
+                        Contact us
+                    </button>
+
+                </div>
+            </div>
+        </section>
     );
 };
 
