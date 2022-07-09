abc2f2ce919ef9eebea77cb93c2d2a9ca2572217
mahfuzswaron <mahfuzswaron@gmail.com>
2022-07-09 13:42:42 +0600

    teatment component added

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,7 +1,19 @@
 import React from 'react';
 import img from '../../images/about/about.png';
-const primary = '#33D687';
-const secondary = '#2A334E';
+import Treatment from './Treatment';
+// const primary = '#33D687';
+// const secondary = '#2A334E';
+const transplante = {
+    name: "Heart Transplante",
+    description: "Heart symptoms call us for immediate",
+    icon: 'icon here'
+}
+const sergery = {
+    name: "Heart Sergery",
+    description: "Heart is get replaced with healthier",
+    icon: 'icon here'
+}
+
 const About = () => {
     return (
         <section className='p-3 lg:p-20 grid grid-cols-1 lg:grid-cols-2 gap-3'>
@@ -16,14 +28,9 @@ const About = () => {
                 </h2>
                 <p className='text-[#8d8d8d] mb-4'>Chest pain is the most common warning sign of a heart attack. But there can be other symptoms, too, like lightheadedness of breath.
                 </p>
-                <div>
-                    <div>
-                        <div>icon here</div>
-                        <div>
-                            <h4>Heart Transplante</h4>
-                            <p>Heart symptoms call us for immediate</p>
-                        </div>
-                    </div>
+                <div className='my-5 grid grid-cols-1 lg:grid-cols-2'>
+                    <Treatment treatment={transplante} />
+                    <Treatment treatment={sergery} />
 
                 </div>
                 <hr />
