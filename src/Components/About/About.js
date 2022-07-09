bd5eb60ab895aa184ac078517d69944db476a21e
mahfuzswaron <mahfuzswaron@gmail.com>
2022-07-09 12:59:40 +0600

    title and paragrap done

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import img from '../../images/about/about.png';
-
+const primary = '#33D687';
+const secondary = '#2A334E';
 const About = () => {
     return (
         <section className='p-3 lg:p-20 grid grid-cols-1 lg:grid-cols-2 gap-3'>
@@ -10,10 +11,10 @@ const About = () => {
                 </figure>
             </div>
             <div>
-                <p>ABOUT Dezily</p>
-                <h2>Heart Surgery Specialist Expert Doctors
+                <p className={`text-[#33D687] font-bold mb-4`}>ABOUT Dezily</p>
+                <h2 className='font-saira text-[2.5rem] text-[#2A334E] font-semibold leading-tight mb-4'>Heart Surgery Specialist Expert Doctors
                 </h2>
-                <p>Chest pain is the most common warning sign of a heart attack. But there can be other symptoms, too, like lightheadedness of breath.
+                <p className='text-[#8d8d8d] mb-4'>Chest pain is the most common warning sign of a heart attack. But there can be other symptoms, too, like lightheadedness of breath.
                 </p>
                 <div>
                     <div>
