07eddab6fc0a8da3a4a355af57d962b472b1f86e
Minhazul Alam <78158511+akib-chy@users.noreply.github.com>
2022-07-09 13:29:43 +0600

    fotter section maked done

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,9 +1,14 @@
 import React from "react";
 import "./Footer.css";
+import FooterImg1 from "../../images/footer/footer-post-01.jpg";
+import FooterImg2 from "../../images/footer/footer-post-02.jpg";
 
 const Footer = () => {
+  const time = new Date();
+  const year = time.getFullYear();
   return (
     <footer>
+      {/* START FOOTER TOP PART */}
       <div className="footer-top-part">
         <div className="container mx-auto p-5  block lg:flex items-center	justify-between">
           <div>
@@ -22,7 +27,7 @@ const Footer = () => {
               </div>
             </div>
           </div>
-          <div>
+          <div className="mt-7 lg:mt-0">
             <p className="text-gray-400">With More 25+ Hospitals</p>
             <p className="text-gray-400">In United States,Europe, and Asia</p>
             <div class="divider"></div>
@@ -30,15 +35,16 @@ const Footer = () => {
               25<sup>+</sup> Hospitals
             </h1>
           </div>
-          <div>
+          <div className="mt-7 lg:mt-0">
             <button className="btn footer-location-btn">
               View Our Location
             </button>
           </div>
         </div>
       </div>
+      {/* START FOOTER MIDDLE PART */}
       <div className="footer-middle-part">
-        <div className="container mx-auto p-5 grid grid-cols-4 gap-4">
+        <div className="container mx-auto p-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
           <div>
             <h2 className="font-bold text-xl mb-4">About Dezily</h2>
             <p className="text-gray-400 pr-5">
@@ -46,12 +52,36 @@ const Footer = () => {
               clinically advanced restorative heart ralated treatments
               available.
             </p>
+            <div className="mt-12">
+              <ul className="footer-social-icon flex items-center">
+                <li>
+                  <a href="#">
+                    <i class="fa-brands fa-facebook-f"></i>
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <i class="fa-brands fa-linkedin-in"></i>
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <i class="fa-brands fa-twitter"></i>
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <i class="fa-brands fa-pinterest-p"></i>
+                  </a>
+                </li>
+              </ul>
+            </div>
           </div>
           <div>
             {" "}
             <h2 className="font-bold text-xl mb-3">Useful Links</h2>
             <div className="flex ">
-              <ul className="pr-10">
+              <ul className="pr-20 lg:pr-14">
                 <li>
                   <a href="#">Home</a>
                 </li>
@@ -93,6 +123,26 @@ const Footer = () => {
           <div>
             {" "}
             <h2 className="font-bold text-xl mb-3">Popular Posts</h2>
+            <div className="flex items-center popular-post p-2">
+              <img src={FooterImg1} alt="" className="mr-4" />
+              <div>
+                <h5>April 17, 2021</h5>
+
+                <a href="#">
+                  <h6>Can Depression Increase Of Your Risk.</h6>
+                </a>
+              </div>
+            </div>
+            <div className="flex items-center popular-post mt-3 p-2">
+              <img src={FooterImg2} alt="" className="mr-4" />
+              <div>
+                <h5>April 17, 2021</h5>
+
+                <a href="#">
+                  <h6>Heart Transplant For The Development Services.</h6>
+                </a>
+              </div>
+            </div>
           </div>
           <div>
             {" "}
@@ -110,15 +160,17 @@ const Footer = () => {
           </div>
         </div>
       </div>
-      <div className="footer-bottom-part">
-        <div className="container mx-auto p-5">
+      {/* START FOOTER BOTTOM PART */}
+      <div className="footer-bottom-part text-center">
+        <div className="container mx-auto p-5 block lg:flex lg:justify-center lg:justify-between">
           <div>
-            <p>
-              Copyright © 2022 <a href="#">Dezaly</a> . All rights reserved
+            <p className="text-center mb-6 lg:mb-0">
+              Copyright &coly; {year} <a href="#">Dezaly</a> . All rights
+              reserved
             </p>
           </div>
           <div>
-            <ul className="flex">
+            <ul className="flex items-center justify-center">
               <li>
                 <a href="#">About Us</a>
               </li>
@@ -134,7 +186,6 @@ const Footer = () => {
               <li>
                 <a href="#">Contact Us</a>
               </li>
-              <li>|</li>
             </ul>
           </div>
         </div>
