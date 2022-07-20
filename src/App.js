40c71a645f7f0fa0979ebe281cf399f5b4413bf0
Minhazul Alam <78158511+akib-chy@users.noreply.github.com>
2022-07-20 12:11:25 +0600

    minhajul navbar issue fix

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
 import "./App.css";
-import WhatWeDo from "./Components/WhatWeDo/WhatWeDo";
-
+import Navbar from "./Components/Navbar/Navbar";
+import Footer from "./Components/Footer/Footer";
+// import WhatWeDo from "./Components/WhatWeDo/WhatWeDo";
+// import Banner from "./Components/Banner/Banner";
+// import About from "./Components/About/About";
+// import About from "./Components/About/About";
+import { Routes, Route } from "react-router-dom";
 function App() {
-  return <div className="App"></div>;
+  return (
+    <>
+      <div className="">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Footer />} />
+        </Routes>
+        {/* <Banner /> */}
+        {/* <About /> */}
+        {/* <WhatWeDo /> */}
+        <Footer />
+      </div>
+    </>
+  );
 }
 
 export default App;
